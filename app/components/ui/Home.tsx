dbcf791377140b622ec14774ef705f5973b158ea
Rahul Hoque <rhoque@umich.edu>
2024-05-17 17:04:13 -0400

    adjusted sizes

--- app/components/ui/Home.tsx
+++ app/components/ui/Home.tsx
@@ -4,11 +4,11 @@ import { BackgroundBeams } from "../../utils/background-beams";
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-end z-50 px-2 h-[51rem] bg-black'> {/* Increase height here */}
-         <h1 className=' text-white animate-fade-right animate-delay-[1000ms] ease-in text-9xl leading-none'>Rahul</h1>
-         <h1 className=' text-black animate-fade-right ease-in animate-delay-[1000ms] text-9xl outline text-transparent leading-none'>Hoque</h1> 
-         <BackgroundBeams className='h-[40rem]' />
-         <div className='h-[10rem]'></div> {/* Add spacer div here */}
+    <div className='flex flex-col justify-end z-50 px-2 sm:h-[41rem] md:h-[51rem] lg:h-[61rem] xl:h-[71rem] 2xl:h-[81rem] bg-black'> {/* Increase height here */}
+         <h1 className=' text-white animate-fade-right animate-delay-[1000ms] ease-in sm:text-7xl md:text-9xl lg:text-[11rem] xl:text-[13rem] 2xl:text-[15rem] leading-none'>Rahul</h1>
+         <h1 className=' text-black animate-fade-right ease-in animate-delay-[1000ms] sm:text-7xl md:text-9xl lg:text-[11rem] xl:text-[13rem] 2xl:text-[15rem] outline text-transparent leading-none'>Hoque</h1> 
+         <BackgroundBeams className='sm:h-[30rem] md:h-[40rem] lg:h-[50rem] xl:h-[60rem] 2xl:h-[70rem]' />
+         <div className='sm:h-[5rem] md:h-[10rem] lg:h-[15rem] xl:h-[20rem] 2xl:h-[25rem]'></div> {/* Add spacer div here */}
 
     </div>
   )
--- app/components/ui/NavBar.tsx
+++ app/components/ui/NavBar.tsx
@@ -11,13 +11,13 @@ import '../../styles/active.scss'
 export default function NavBar() {
     return (
         <div className='fixed top-0 z-50 bg-inherit backdrop-blur-md w-full'>
-          <div className=' border-[#606060] border-b-[1px] px-5 lg:px-7.5 xl:px-10 max-lg: py-4'> 
-            <nav className='flex justify-between static top-[5rem] left-0'>
-              <div className='relative z-2 flex space-x-4 font-mono text-[#606060]'>
-                <a className='pr-7' href='#hero'>
-                  <Image src={RH} height={40} width={40} alt='logo'/>
+          <div className='border-[#606060] border-b-[1px] px-2 sm:px-3 md:px-5 lg:px-7.5 xl:px-10 2xl:px-12 py-2 sm:py-3 md:py-4 lg:py-5 xl:py-6 2xl:py-7'> 
+            <nav className='flex justify-between static top-2 sm:top-3 md:top-4 lg:top-5 xl:top-6 2xl:top-7 left-0'>
+              <div className='relative z-2 flex space-x-2 sm:space-x-3 md:space-x-4 lg:space-x-5 xl:space-x-6 2xl:space-x-7 font-mono text-[#606060]'>
+                <a className='pr-2 sm:pr-3 md:pr-4 lg:pr-5 xl:pr-6 2xl:pr-7' href='#hero'>
+                <Image src={RH} height={40} width={40} alt='logo'/>
                 </a>
-                <ul className='flex space-x-4'>
+                <ul className='flex space-x-2 sm:space-x-3 md:space-x-4 lg:space-x-5 xl:space-x-6 2xl:space-x-7'>
                   <button><Link activeClass='active' smooth spy to="home" className='hover:font-extrabold'>Home</Link></button>
                   <button><Link activeClass='active' smooth spy to="experience" className='hover:font-extrabold'>Experience</Link></button>
                   <button><Link activeClass='active' smooth spy to="projects" className='hover:font-extrabold'>Projects</Link></button>
@@ -25,9 +25,9 @@ export default function NavBar() {
                 </ul>
               </div>
               <IconContext.Provider value={{ color: 'white' }}>
-                <div className='flex space-x-4'>
-                  <a href='https://www.linkedin.com/in/rahul-hoque-6a9a6b282/' target="_blank" rel="noopener noreferrer"><FaLinkedin size={20}/></a>
-                  <a href='https://github.com/HoqueUM' target="_blank" rel="noopener noreferrer"><FaGithub size={20}/></a>
+                <div className='flex space-x-2 sm:space-x-3 md:space-x-4 lg:space-x-5 xl:space-x-6 2xl:space-x-7'>
+                <a href='https://www.linkedin.com/in/rahul-hoque-6a9a6b282/' target="_blank" rel="noopener noreferrer"><FaLinkedin size={20}/></a>
+                <a href='https://github.com/HoqueUM' target="_blank" rel="noopener noreferrer"><FaGithub size={20}/></a>
                 </div>
               </IconContext.Provider>
             </nav>
