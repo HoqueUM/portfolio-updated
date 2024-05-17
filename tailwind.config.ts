57b4db311454f6a4e8178cc4c24a8a280bc351d1
Rahul Hoque <rhoque@umich.edu>
2024-05-17 18:01:52 -0400

    please

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,13 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px', 
+      'lg': '1024px', 
+      'xl': '1280px', 
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         "primary-color": "var(--primary-color)",
