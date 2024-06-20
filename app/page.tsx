3b4035de5e1041aea408de14e6a4f0760a6e1fdd
Rahul Hoque <rhoque@umich.edu>
2024-06-19 22:29:52 -0400

    made a card

--- app/page.tsx
+++ app/page.tsx
@@ -15,22 +15,6 @@ import ProjectCard from './components/model/ProjectCard';
 const inter = Inter({ weight: "400", subsets: ["latin"] });
 
 export default function Page() {
-  const projects = [
-    {
-      image: '/home/rhoque/portfolio-updated/app/components/ui/favicon.png',
-      name: 'Poopy Bum',
-      description: 'Hi',
-      skills: ['React', 'Next.js', 'Tailwind CSS'],
-      link: 'https://github.com/HoqueUM/better-atlas',
-    },
-    {
-      image: '/home/rhoque/portfolio-updated/app/components/ui/favicon.png',
-      name: 'Cock',
-      description: 'I hate CS!',
-      skills: ['React', 'Next.js', 'Tailwind CSS'],
-      link: 'https://github.com/HoqueUM/better-atlas',
-    },
-  ];
   return (
 
     <main className={inter.className}>   
@@ -47,17 +31,8 @@ export default function Page() {
       </div>
       <div className='flex flex-col h-screen justify-center items-center bg-black text-secondary-color text-center' id='projects'>
         
-        <p className='mb-2'> ⌛ Complete ⌛</p>
-        {projects.map((project, index) => (
-          <ProjectCard
-            key={index}
-            image={project.image}
-            name={project.name}
-            description={project.description}
-            skills={project.skills}
-            link={project.link}
-          />
-        ))}
+        <p className='mb-2'> ✅ Complete ✅</p>
+
         <p className='mb-2'> ⌛ Coming soon ⌛</p>
         <p>Stay tuned to see what I&apos;m working on!</p>
             </div>
